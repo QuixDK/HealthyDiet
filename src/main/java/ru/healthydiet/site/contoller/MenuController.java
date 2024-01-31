75299698564cd1ba0774afa037544703c14d1f1f
QuixDK <stroganovdanila@outlook.com>
2024-01-31 19:55:59 +0700

    develop add service class

--- src/main/java/ru/healthydiet/site/contoller/MenuController.java
+++ src/main/java/ru/healthydiet/site/contoller/MenuController.java
@@ -10,6 +10,7 @@ import ru.healthydiet.site.DTO.ProductDTO;
 import ru.healthydiet.site.model.Product;
 import ru.healthydiet.site.model.ProductContent;
 import ru.healthydiet.site.repository.ProductRepo;
+import ru.healthydiet.site.service.ProductService;
 
 import java.util.List;
 
@@ -18,70 +19,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MenuController {
 
-    @Autowired
-    private final ProductRepo productRepo;
+    private final ProductService productService;
     private final ObjectMapper objectMapper;
 
     @PostMapping("/api/add")
     public void addProduct(@RequestBody ProductDTO productDTO) {
-        log.info(
-                "Add new product: " + productRepo.save(
-                        Product.builder()
-                                .name(productDTO.getName())
-                                .productContent(ProductContent.builder()
-                                        .calories(productDTO.getCalories())
-                                        .proteins(productDTO.getProteins())
-                                        .fats(productDTO.getFats())
-                                        .carbohydrates(productDTO.getCarbohydrates())
-                                        .fibers(productDTO.getFibers())
-                                        .sugar(productDTO.getSugar())
-                                        .sodium(productDTO.getSodium())
-                                        .cuprum(productDTO.getCuprum())
-                                        .iron(productDTO.getIron())
-                                        .folate(productDTO.getFolate())
-                                        .potassium(productDTO.getPotassium())
-                                        .magnesium(productDTO.getMagnesium())
-                                        .thiamine(productDTO.getThiamine())
-                                        .riboflavin(productDTO.getRiboflavin())
-                                        .vitaminB6(productDTO.getVitaminB6())
-                                        .vitaminC(productDTO.getVitaminC())
-                                        .vitaminE(productDTO.getVitaminE())
-                                        .build())
-                                .build())
-        );
+        productService.saveNewProduct(productDTO);
     }
 
     @SneakyThrows
     @GetMapping("/api/getProductContent")
     public String getProductContent(@RequestParam Integer id)
     {
-        if (!productRepo.existsById(id)) {
-            return "No such product";
-        }
-        return objectMapper.writeValueAsString(productRepo.findById(id).get().getProductContent());
+        return productService.getProductContent(id);
     }
 
     @GetMapping("/api/getAll")
     public List<Product> getAllProducts() {
-        return productRepo.findAll();
+        return productService.getAllProducts();
     }
 
     @DeleteMapping("/api/delete")
     public String deleteProduct(@RequestParam Integer id) {
-        if (!productRepo.existsById(id)) {
-            return "No such product";
-        }
-        productRepo.deleteById(id);
-        log.info("Delete product with id: " + id);
-        return "Delete product with id: " + id;
+        return productService.deleteProduct(id);
     }
 
     @PutMapping("/api/update")
     public String updateProduct(@RequestBody Product product) {
-        if (!productRepo.existsById(product.getId())) {
-            return "No such product";
-        }
-        log.info("Update product: " + productRepo.save(product));
-        return "Update product: " + productRepo.findById(product.getId());
+        return productService.updateProduct(product);
     }
 }
